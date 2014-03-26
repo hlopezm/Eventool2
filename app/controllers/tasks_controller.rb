547518a0b357b8cc37fe3213790b81e484d35c81
hlopez <hlopezm23@gmail.com>
2014-03-26 22:38:06 +0100

    fixing top menu

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,30 +5,45 @@ class TasksController < ApplicationController
     @complete_tasks = Task.find(:all, :conditions => "finish IS NOT NULL")
     @tasks = Task.all
   end
+
   def show
-    @task = Task.find(id)
-  end
-  def new
-    @task = tasks.build
+  	@task = Task.find(params[:id])
   end
 
-
-#  def complete
-#  Task.update_all(["finish=?", True, :id => params[:task_ids])
-#end
-
-  private
-
-  def set_task
-    @event = Task.find params[:id]
+  def edit
+  	@task = Task.find(params[:id])
   end
 
-  def create_params
-    params.require(:task).permit(*policy(Task).new_permitted_attrs)
+  def update
+      @task = Task.find(params[:id])
+      if @task.update edit_parameters
+        redirect_to root_path
+      else
+        render 'edit'
+      end
+    end
+
+  #  def complete
+  #  Task.update_all(["finish=?", True, :id => params[:task_ids])
+  #end
+
+    private
+
+    def set_task
+      @event = Task.find params[:id]
+    end
+
+    def create_params
+      params.require(:task).permit(*policy(Task).new_permitted_attrs)
+    end
+
+    def update_params
+      params.require(:task).permit(*policy(@task).edit_permitted_attrs)
+    end
+
+    def edit_parameters
+      params.require(:task).permit(:admin)
   end
 
-  def update_params
-    params.require(:task).permit(*policy(@task).edit_permitted_attrs)
-  end
 
 end
